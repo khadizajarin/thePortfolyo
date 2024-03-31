c3aed8c9d0832b0eee86a3a7888e573c685fd84a
khadizajarin <khadiza131310@gmail.com>
2024-03-31 13:17:51 +0600

    timeline

--- src/componants/Timeline/TimeLines.jsx
+++ src/componants/Timeline/TimeLines.jsx
@@ -1,78 +1,47 @@
 
-import { forwardRef, useEffect, useRef, useState } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './Timeline.css'
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
 import {motion} from 'framer-motion'
 
-gsap.registerPlugin(ScrollTrigger);
+const circleRadius = 80; 
 
+const draw = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i) => {
+        const delay = 1 + i * 0.75;
+        return {
+            pathLength: 1,
+            opacity: 1,
+            rotate: 360, 
+            transition: {
+                pathLength: { delay, type: "spring", duration: 3,  repeat: Infinity},
+                opacity: { delay, duration: 3 },
+                rotate: { delay, duration: 3, repeat: Infinity },
+            }
+        };
+    }
+};
+
+const circles = Array.from({ length: 3 }, (_, i) => (
+    <motion.circle
+        key={i}
+        cx="300"
+        cy="200"
+        r={circleRadius} 
+        stroke="#76ABAE"
+        variants={draw}
+        custom={i + 1}
+    />
+));
 
 // eslint-disable-next-line react/display-name
 const Timelines = forwardRef((props, ref) => {
 
-    useEffect(() => {
-        AOS.init();
-      }, []);
-
-     // Function to format date
      const formatDate = (dateString) => {
         const date = new Date(dateString);
         return date.toLocaleDateString('en-US', { year: 'numeric', month: 'short' });
     };
-
-    // useEffect(() => {
-       
-    //     // Animations for Education section
-    //     gsap.from('.education-date', {
-    //         scrollTrigger: {
-    //             trigger: '.education-section',
-    //             start: 'top center',
-    //             end: 'bottom center',
-    //             scrub: true
-    //         },
-    //         scaleX: 1.5,
-    //         transformOrigin: 'left',
-    //         ease: 'none'
-    //     });
-
-    //     // Animations for Experience section
-    //     gsap.from('.experience-date', {
-    //         scrollTrigger: {
-    //             trigger: '.experience-section',
-    //             start: 'top center',
-    //             end: 'bottom center',
-    //             scrub: true
-    //         },
-    //         scaleX: 1.5,
-    //         transformOrigin: 'right',
-    //         ease: 'none'
-    //     });
-    // }, []);
-    
-    const boxRef = useRef(null);
-    useEffect(() => {
-        gsap.to(".box", {
-            rotation: 180,
-            duration: 5,
-            scrollTrigger: {
-                trigger: ".box",
-                start: "top top", // Start animation when ".box" enters the viewport
-                end: "bottom bottom", // End animation when ".box" leaves the viewport
-                scrub: true,
-                onEnter: () => {
-                    gsap.to(".box", {opacity:0,x:0, y: 500 ,ease: "power2.inOut"});
-                },
-                onLeaveBack: () => {
-                    gsap.to(".box", {opacity:1,x:0, y: 0,ease: "power2.inOut" });
-                }
-            }
-        });
-
-    }, []);
 
 
     const [info, setInfo] = useState([]);
@@ -92,59 +61,95 @@ const Timelines = forwardRef((props, ref) => {
 
     return (
         <div ref={ref} className="">
+            <hr className="divider"/>
             <h1 className="text-center font-bold text-4xl lg:text-5xl text-[#76ABAE] my-20">Timeline...</h1>
             <div className="">
-            <div className="flex justify-between">
+            <div className="lg:flex justify-between">
                 {/* Education tab content */}
                 <div className="w-1/2 px-20 education-section">
                     <h2 className="text-center font-bold text-4xl text-[#76ABAE] mb-4">Education</h2>
                     {info.slice().reverse().filter(item => item.forEducation).slice().reverse().map((line, index) => (
                         <div key={index} className="py-10 relative">
-                            <div className="text-right text-[#76ABAE]">
+                            <div className="flex flex-row-reverse  items-center">
+                            <motion.svg
+                                width="300"
+                                height="300"
+                                viewBox="0 0 600 600"
+                                initial="hidden"
+                                animate="visible"
+                            >
+                                {circles}
+                            </motion.svg>
+                            <div data-aos="fade-down" className="text-right text-[#76ABAE]">
                                 <div className="text-right mb-2">
-                                    <div className="flex flex-row justify-between items-center">
+                                    <div className="flex flex-row justify-between items-center mb-2">
                                         <h2 className="text-right font-extrabold text-3xl">{line.company_name}</h2>
-                                        <p className="education-date">{formatDate(line.startDate)} - {formatDate(line.endDate)}</p>
+                                        <p className="education-date">{formatDate(line.startDate)} - {formatDate(line.endDate)},<br />{line.jobLocation}</p>
                                     </div>
                                     <h2 className='font-extrabold'>{line.summary}</h2>
+                                    <ul className="custom-bullets mt-4">
+                                        {line.bulletPoints.map((bullet, index) => (
+                                            <li key={index}>
+                                                <span className="bullet-icon">•</span>
+                                                {bullet}
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
                                 <p>{line.description}</p>
                             </div>
-                            
+                            </div>
                         </div>
                     ))}
                 </div>
 
-                <div ref={boxRef}  className="trigger">
-                    <div  className="box"></div>
-                </div>
- 
 
-                        
+
+     
                             
                     {/* Experience tab content */}
                 <div className="w-1/2 px-20 experience-section">
-                    <h2 className="text-center font-bold text-4xl text-[#76ABAE] mb-4">Experience</h2>
-                    {info.filter(item => !item.forEducation).slice().reverse().map((line, index) => (
-                        <div key={index} className="py-10 relative">
-                            <div className="text-left text-[#76ABAE]">
-                                <div className="text-left mb-2">
-                                    <div className="flex flex-row-reverse justify-between items-center">
-                                        <h2 className="text-left font-extrabold text-3xl">{line.company_name}</h2>
-                                        <p className="experience-date">{formatDate(line.startDate)} - {formatDate(line.endDate)}</p>
+                        <h2 className="text-center font-bold text-4xl text-[#76ABAE] mb-4">Experience</h2>
+                        {info.filter(item => !item.forEducation).slice().reverse().map((line, index) => (
+                            <div key={index} className="py-10 relative">
+                                <div  className="flex flex-row items-center">
+                                <motion.svg
+                                    width="300"
+                                    height="300"
+                                    viewBox="0 0 600 600"
+                                    initial="hidden"
+                                    animate="visible"
+                                >
+                                    {circles}
+                                </motion.svg>
+                                <div data-aos="fade-down" className="text-left text-[#76ABAE]">
+                                    <div className="text-left mb-2">
+                                        <div className="flex flex-row-reverse justify-between items-center mb-2">
+                                            <h2 className="text-left font-extrabold text-3xl">{line.company_name}</h2>
+                                            <p className="experience-date ">{formatDate(line.startDate)} - {formatDate(line.endDate)}, <br />{line.jobLocation}</p>
+                                        </div>
+                                        <h2 className='font-extrabold text-left'>{line.summary}</h2>
+                                        <ul className="custom-bullets mt-4">
+                                            {line.bulletPoints.map((bullet, index) => (
+                                                <li key={index}>
+                                                    <span className="bullet-icon">•</span> {/* Custom bullet icon */}
+                                                    {bullet}
+                                                </li>
+                                            ))}
+                                        </ul>
                                     </div>
-                                    <h2 className='font-extrabold text-left'>{line.summary}</h2>
+                                    <p className="mt-4">{line.description}</p>
                                 </div>
-                                <p>{line.description}</p>
+                                </div>
+                                
+                                
                             </div>
-                            
-                            
-                        </div>
-                    ))}
+                        ))}
                 </div>
-</div>
+            </div>
 
         </div>
+        <hr className="divider"/>
         </div>
     );
 });
